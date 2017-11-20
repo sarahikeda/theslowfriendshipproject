22c3ecbf3ded44148693b22e54146284101db9fe
Sarah Ing <sarahikeda@gmail.com>
2017-11-20 13:33:01 -0500

    Move App component into a components folder

---
+++ my-template/src/components/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Boilerplate</h1>
+        </header>
+        <p className="App-intro">Intro</p>
+      </div>
+    );
+  }
+}
+
+export default App;
